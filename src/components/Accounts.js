fca1d7a19f1afcde0363d738a125eff7dbd5b32d
jonathanlancaster <jcast90@me.com>
2019-09-08 07:56:46 -0700

    added pagination and rows per page

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { Button, Grid, Table, TableRow, TableHead, TableCell, TableBody } from '@material-ui/core';
+import { Button, Grid, Table, TableRow, TableHead, TableCell, TableBody, TablePagination } from '@material-ui/core';
 
 class Accounts extends Component {
     state = { 
         accounts: [],
-        selectedAccount: 'equifax'
+        selectedAccount: 'equifax',
+        currentPage: 0,
+        rowsPerPage: 5
     }
-
+    
     componentDidUpdate(prev, next) {
         if(prev.reports !== this.props.reports){
             let bureauAccounts = {};
@@ -19,7 +21,7 @@ class Accounts extends Component {
     }
 
     selectedAccount = (bureau = 'equifax') => {
-        this.setState({ selectedAccount: bureau})
+        this.setState({ selectedAccount: bureau, currentPage: 0})
     }
 
     renderButtons = () => {
@@ -42,26 +44,38 @@ class Accounts extends Component {
 
         return null;
     }
+    handleChangePage = (e, newPage) => {
+        this.setState({currentPage: newPage})
+    }
+
+    handleRowChange = (e) => {
+        this.setState({rowsPerPage: e.target.value, currentPage: 0})
+    }
 
     renderAccounts = () => {
         if(this.state.accounts.length > 0){
-            const { accounts, selectedAccount } = this.state;
+            const { accounts, selectedAccount, currentPage, rowsPerPage } = this.state;
 
-            return accounts[0][selectedAccount].map( (account, index) => {
-                return (
-                    <TableRow key={`${account.number}-${index}`} >
-                        <TableCell>{account.name}</TableCell>
-                        <TableCell>{account.number}</TableCell>
-                        <TableCell>{selectedAccount === 'equifax' ? account.balance : '-'}</TableCell>
-                        <TableCell>{selectedAccount === 'transunion' ? account.balance : '-'}</TableCell>
-                        <TableCell>{selectedAccount === 'experian' ? account.balance : '-'}</TableCell>
-                    </TableRow>
-                )
+            return accounts[0][selectedAccount]
+                .slice(currentPage * rowsPerPage, currentPage * rowsPerPage + rowsPerPage)
+                .map( (account, index) => {
+                    return (
+                        <TableRow key={`${account.number}-${index}`} >
+                            <TableCell>{account.name}</TableCell>
+                            <TableCell>{account.number}</TableCell>
+                            <TableCell>{selectedAccount === 'equifax' ? account.balance : '-'}</TableCell>
+                            <TableCell>{selectedAccount === 'transunion' ? account.balance : '-'}</TableCell>
+                            <TableCell>{selectedAccount === 'experian' ? account.balance : '-'}</TableCell>
+                        </TableRow>
+                    )
             })
         }
     }
 
     render() {
+        const { accounts, selectedAccount, currentPage, rowsPerPage } = this.state;
+        const accountLength = accounts[0] ? accounts[0][selectedAccount].length : 0;
+
         return (
             <div>
                 <Grid
@@ -86,6 +100,21 @@ class Accounts extends Component {
                         {this.renderAccounts()}
                     </TableBody>
                 </Table>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={accountLength}
+                    rowsPerPage={rowsPerPage}
+                    page={currentPage}
+                    backIconButtonProps={{
+                        'aria-label': 'previous page',
+                    }}
+                    nextIconButtonProps={{
+                        'aria-label': 'next page',
+                    }}
+                    onChangePage={this.handleChangePage}
+                    onChangeRowsPerPage={this.handleRowChange}
+                />
             </div>
         );
     }
